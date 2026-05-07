5176f695ebc88d18c4fadcb609d8765a897040c8
vikas chauhan <45870320+chvikaschauhan@users.noreply.github.com>
2026-05-07 12:53:20 +0530

    basic login api

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("PulseGuard Local Server Running 🚀");
 });
+
+//LoginAPI
+app.post("/login", (req, res) => {
+ const{email, password} = req.body;
+
+ if (email == "vikas@gmail.com" && password == "0000"){
+    return res.jason({
+        status : "success"
+    })
+ } else {
+    return res.status(401).json({
+        status: "failed"
+    })
+ }
+
+
+  
+});
+
 
 app.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
